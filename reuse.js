ff2b00fe1850b9f250e83c93f8ee4cbb699604ac
Ana Canizales <napyruiz@hotmail.com>
2023-08-29 22:42:42 -0500

    navbar y footer ok

--- reuse.js
+++ reuse.js
@@ -2,7 +2,6 @@
 function displayFooter() {
     const footer = document.createElement('footer');
     footer.innerHTML = `
-    
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -20,63 +19,23 @@ function displayFooter() {
     </head>
     <body>
     
-      <footer class="footer bg-gradient-light-translucent pt-5 pb-4 pb-lg-5" id="footer" >
-        <div class="container pt-lg-4" id="borde">
-          <div class="row pb-5">
-            <div class="col-lg-4 col-md-6">
-              <div class="navbar-brand text-body-tertiary p-0 me-0 mb-3 mb-lg-4">
-                <img src="img/log larg.png" alt="Logo" id="logoNormal">
-                
-              </div>
-             
-            </div>
-            <div class="col-xl-6 col-lg-7 col-md-5 offset-xl-2 offset-md-1 pt-4 pt-md-1 pt-lg-0">
-              <div id="footer-links" class="row">
-                <div class="col-lg-4">
-                  <h6 class="mb-2">
-                    <a href="#useful-links" class="d-block text-dark dropdown-toggle d-lg-none py-2" data-bs-toggle="collapse" id="anim">Ccs Facilities Managmen UK</a>
-                  </h6>
-                  <div id="useful-links" class="collapse d-lg-block" data-bs-parent="#footer-links">
-                    <ul class="nav flex-column pb-lg-1 mb-lg-3">
-                      <li class="link-secondary"><a id="animf" href="index.html" class="nav-link d-inline-block px-0 pt-1 pb-2 link-secondary">Home</a></li>
-                      <li class=""><a id="animf" href="how-we-are.html" class="nav-link d-inline-block px-0 pt-1 pb-2 link-secondary">About Us</a></li>
-                      <li class=""><a id="animf" href="recruitment.html" class="nav-link d-inline-block px-0 pt-1 pb-2 link-secondary">Recruitment</a></li>
-                      <li class=""><a id="animf" href="contact-us.html" class="nav-link d-inline-block px-0 pt-1 pb-2 link-secondary">Contact Us</a></li>
-                      
-                    </ul>
-                    <ul class="nav flex-column mb-2 mb-lg-0">
-                      <li class="nav-item"><a id="animf" href="#" class="nav-link d-inline-block px-0 pt-1 pb-2 link-secondary">Terms &amp; Conditions</a></li>
-                      <li class="nav-item"><a id="animf" href="#" class="nav-link d-inline-block px-0 pt-1 pb-2 link-secondary">Privacy Policy</a></li>
-                    </ul>
-                  </div>
-                </div>
-                <div class="col-xl-4 col-lg-3">
-                  <h6 class="mb-2">
-                    <a href="#social-links" class="d-block text-dark dropdown-toggle d-lg-none py-2" data-bs-toggle="collapse" id="anim">Socials</a>
-                  </h6>
-                  <div id="social-links" class="collapse d-lg-block" data-bs-parent="#footer-links">
-                    <ul class="nav flex-column mb-2 mb-lg-0">
-                      <li class="nav-item"><a id="animf" href="#" class="nav-link d-inline-block px-0 pt-1 pb-2 link-secondary">Facebook</a></li>
-                      <li class="nav-item"><a id="animf" href="https://www.linkedin.com/in/victor-j-estrada-89b314275/" class="nav-link d-inline-block px-0 pt-1 pb-2 link-secondary">LinkedIn</a></li>
-                      <li class="nav-item"><a id="animf" href="#" class="nav-link d-inline-block px-0 pt-1 pb-2 link-secondary">Twitter</a></li>
-                      <li class="nav-item"><a id="animf" href="#" class="nav-link d-inline-block px-0 pt-1 pb-2 link-secondary">Instagram</a></li>
-                    </ul>
-                  </div>
-                </div>
-                <div class="col-xl-4 col-lg-5 pt-2 pt-lg-0">
-                  <a href="mailto:email@example.com" class="fw-medium">email@example.com</a>
-                  <button type="button" class="btn btn-outline-success" id="botonNumero2" onclick="btnOnClick2()" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="click to copy number">123-456-789</button>
-                  <a href="https://api.whatsapp.com/send?phone=573217043952&text=Hey!%20I%20Need%20A%20Quote%20" target="_blank" rel="noopener noreferrer">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/479px-WhatsApp_icon.png" alt="iconoWhatsapp" height="25px" width="25px">
-                   </a>
-                </div>
-              </div>
-            </div>
-          </div>
-         
-        </div>
-      </footer>
+      <div class="border-top">
+        <footer class="container d-flex flex-wrap justify-content-between align-items-center py-3 my-4">
+          <p class="col-md-4 mb-0 text-muted">© 2023 facilities management</p>
       
+          <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+            <img id="navImg" src="img/icon-lo.png" alt="logo" height="80px" width="70px" >
+          </a>
+      
+          <ul class="nav col-md-4 justify-content-end">
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About Us</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Recruitment</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Contact</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+          </ul>
+        </footer>
+      </div>
     
     </body>
     </html>
@@ -89,37 +48,96 @@ function displayFooter() {
   function displayNavbar() {
     const nav = document.createElement('nav');
     nav.innerHTML = `
-    <nav class="navbar navbar-expand-lg bg-body-tertiary justify-content-center">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown link
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+        <title>Document</title>
+    </head>
+    <body>
+    
+        <nav class="navbar navbar-expand-lg bg-body-tertiary justify-content-center" id="elnav">
+            <div class="container-fluid justify-content-center container">
+
+                
+                <a class="navbar-brand text-center" href="index.html">
+                    <img id="navImg" src="img/icon-lo.png" alt="logo" height="80px" width="70px" >
+                </a> 
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
+                
+                <ul class="navbar-nav">
+                
+                  <li class="nav-item">
+                    <a id="txtNav" class="nav-link active" aria-current="page" href="index.html">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a id="txtNav" class="nav-link" href="how-we-are.html">About us</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a id="txtNav" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Cleaning Services
+                    </a>
+                    <ul class="dropdown-menu text-center" >
+                        <li><a id="txtNavHijo" class="dropdown-item" href="">Maintenance services</a></li>
+                        <li><a id="txtNavHijo" class="dropdown-item" href="">Consumables and janitorial supplies</a></li>
+                        <li><a id="txtNavHijo" class="dropdown-item" href="">Office cleaning</a></li>
+                        <li><a id="txtNavHijo" class="dropdown-item" href="">Carpet c</a></li>
+                        <li><a id="txtNavHijo" class="dropdown-item" href="">Windo c reach and wash</a></li>
+                        <li><a id="txtNavHijo" class="dropdown-item" href="">Jetting wash services</a></li>
+                        <li><a id="txtNavHijo" class="dropdown-item" href="">Graffiti removal</a></li>
+                        <li><a id="txtNavHijo" class="dropdown-item" href="">Afther building</a></li>
+                        <li><a id="txtNavHijo" class="dropdown-item" href="">One off</a></li>
+                        <li><a id="txtNavHijo" class="dropdown-item" href="">Afther party cleaning</a></li>
+                        <li><a id="txtNavHijo" class="dropdown-item" href="">Residential and domestic.</a></li>                        
+                    </ul>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a id="txtNav" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Maintenance Services
+                    </a>
+                    <ul class="dropdown-menu text-center" >
+                        <li><a id="txtNavHijo" class="dropdown-item" href="">Maintenance</a></li>
+                        <li><a id="txtNavHijo" class="dropdown-item" href="">Plumbing services</a></li>
+                        <li><a id="txtNavHijo" class="dropdown-item" href="">Electricity services</a></li>
+                        <li><a id="txtNavHijo" class="dropdown-item" href="">Painting and decorating</a></li>
+                        <li><a id="txtNavHijo" class="dropdown-item" href="">Tailing.</a></li>
+                        <li><a id="txtNavHijo" class="dropdown-item" href="">Guttering clean and repair service.</a></li>
+                        <li><a id="txtNavHijo" class="dropdown-item" href="">Dismantle and Unsamble services.</a></li>
+                        <li><a id="txtNavHijo" class="dropdown-item" href="">Jet washed services</a></li>
+                        <li><a id="txtNavHijo" class="dropdown-item" href="">Wasted removal </a></li>
+                        <li><a id="txtNavHijo" class="dropdown-item" href="">Rubbish collection.</a></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a id="txtNav" class="nav-link" href="recruitment.html">Recruitment</a>
+                  </li>
+                  <li class="nav-item">
+                    <a id="txtNav" class="nav-link" href="contact-us.html">Contact Us</a>
+                  </li>
+
+
                 </ul>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+
+                
+                <a href="https://api.whatsapp.com/send?phone=573217043952&text=Hey!%20I%20Need%20A%20Quote%20" target="_blank" rel="noopener noreferrer">
+                    <img id="margeniconow" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/479px-WhatsApp_icon.png" alt="iconoWhatsapp" height="50px" width="50px">
+                </a>
+
+              </div>
+            </div>
+          </nav>
+    
+
+   
+
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    </body>
+    </html>
     `;
     document.body.appendChild(nav);
   }
