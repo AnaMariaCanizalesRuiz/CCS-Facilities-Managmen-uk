9cc806ef0286c5449384098518c78c0269052314
Ana Canizales <napyruiz@hotmail.com>
2023-08-30 21:18:28 -0500

    Navbar listo colores + cambio display 5 a 6 y 4 a 5

--- js/jsIndex.js
+++ js/jsIndex.js
@@ -1,39 +1,3 @@
-var numero= 123456789;
-var boton2= document.getElementById('botonNumero');
-
-
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-
-
-function btnOnClick(){
-    var aux = document.createElement("input");
-    aux.setAttribute("value", numero);
-    document.body.appendChild(aux);
-    aux.select();
-    document.execCommand("copy");
-    document.body.removeChild(aux);
-    const tooltip = bootstrap.Tooltip.getInstance('#botonNumero1') // Returns a Bootstrap tooltip instance
-    // setContent example
-    tooltip.setContent({ '.tooltip-inner': 'copied number' })
-    
-}
-
-
-function btnOnClick2(){
-    console.log( "Hola soy btnOnClick2" );
-    var aux = document.createElement("input");
-    aux.setAttribute("value", numero);
-    document.body.appendChild(aux);
-    aux.select();
-    document.execCommand("copy");
-    document.body.removeChild(aux);
-    const tooltip2 = bootstrap.Tooltip.getInstance('#botonNumero2') // Returns a Bootstrap tooltip instance
-    // setContent example
-    tooltip2.setContent({ '.tooltip-inner': 'copied number' })
-    
-}
-
 window.addEventListener('scroll', function(){
     let animacion = document.getElementById('Seccion2');
     let posicionObj = animacion.getBoundingClientRect().top;
